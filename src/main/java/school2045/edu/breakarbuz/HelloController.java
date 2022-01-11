68bca5bdd8549508b5198358ffa784957ee410b6
piolm <piolm.p.r.o16@gmail.com>
2022-01-11 18:53:40 +0300

    made som dings

--- src/main/java/school2045/edu/breakarbuz/HelloController.java
+++ src/main/java/school2045/edu/breakarbuz/HelloController.java
@@ -4,11 +4,42 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    int clickedLunka, neededLunka;
+
+    public void mainMethod() {
+
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    protected void Lunka1() {}
+    @FXML
+    protected void Lunka3() {}
+    @FXML
+    protected void Lunka2() {}
+    @FXML
+    protected void Lunka4() {}
+    @FXML
+    protected void Lunka5() {}
+    @FXML
+    protected void Lunka6() {}
+    @FXML
+    protected void Lunka7() {}
+    @FXML
+    protected void Lunka8() {}
+    @FXML
+    protected void Lunka9() {}
+    @FXML
+    protected void Lunka10() {}
+    @FXML
+    protected void Lunka11() {}
+    @FXML
+    protected void Lunka12() {}
+    @FXML
+    protected void Lunka13() {}
+    @FXML
+    protected void Lunka14() {}
+    @FXML
+    protected void Lunka15() {}
+    @FXML
+    protected void Lunka16() {}
 }
