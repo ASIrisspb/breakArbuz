a0e601b385c17f6f5cad3bbfd2d0313a5045b784
ALEXANDR IVANOV <39660147+ASIrisspb@users.noreply.github.com>
2022-01-11 17:51:13 +0300

    This test game for work

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module school2045.edu.breakarbuz {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens school2045.edu.breakarbuz to javafx.fxml;
+    exports school2045.edu.breakarbuz;
+}
---
+++ src/main/java/school2045/edu/breakarbuz/HelloApplication.java
@@ -0,0 +1,23 @@
+package school2045.edu.breakarbuz;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        stage.setTitle("Break the Arbuz");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
